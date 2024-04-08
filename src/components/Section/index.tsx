a83c0a626f7e9de46fc39f45f9074a71a747a483
Luciano Lucio Neto <lucianorlneto@more.tt>
2024-04-08 20:30:38 -0300

    feat: sets Section title to uppercase

--- src/components/Section/index.tsx
+++ src/components/Section/index.tsx
@@ -10,7 +10,7 @@ const Section: React.FC<SectionProps> = ({ children, index, title }) => {
   return (
     <div className="justify-start mb-12">
       <p className="font-black text-2xl underline underline-offset-8 text-left">
-        {index} - {title}
+        {index} - {title.toUpperCase()}
       </p>
       {children}
     </div>
