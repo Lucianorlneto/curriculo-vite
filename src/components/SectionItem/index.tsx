02791359ac66d0676a67d0763e008cebdca1ffad
Luciano Lucio Neto <lucianorlneto@more.tt>
2024-04-09 18:02:01 -0300

    feat: adds time to SectionItem

--- src/components/SectionItem/index.tsx
+++ src/components/SectionItem/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 
 interface SectionItemProps {
   period?: string;
+  time?: string;
   title?: string;
   city?: string;
   description?: React.ReactElement;
@@ -10,14 +11,20 @@ interface SectionItemProps {
 const SectionItem: React.FC<SectionItemProps> = ({
   city,
   period,
+  time,
   title,
   description,
 }) => {
   return (
     <div className="flex flex-row pt-6 mb-4">
-      <p className="font-bold min-w-64 underline text-left items-end mt-1">
-        {period}
-      </p>
+      <div className="flex flex-col min-w-64 items-start justify-start border-red-700 left-0">
+        <div>
+          <p className="inline-block font-bold underline text-left items-end mt-1">
+            {period}
+          </p>
+          {time && <p className="text-center items-end mt-1">{time}</p>}
+        </div>
+      </div>
       <div className="flex-1">
         <div className="flex w-full flex-row mb-1">
           <p className="flex flex-1 font-bold text-2xl text-left mr-8">
