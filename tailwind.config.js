53d7193b55ceb9d694031c636e67943944246b0b
Luciano Lucio Neto <lucianorlneto@more.tt>
2024-04-09 18:03:33 -0300

    style: changes global fontSizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontSize: {
+      sm: "0.8rem",
+      base: "1rem",
+      xl: "1.25rem",
+      "2xl": "1.3rem",
+      "3xl": "1.653rem",
+      "4xl": "1.841rem",
+      "5xl": "2.252rem",
+    },
     extend: {},
   },
   plugins: [],
