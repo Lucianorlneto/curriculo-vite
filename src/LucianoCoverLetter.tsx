6d322d91890d0fc672d0fd3e923607a0da63d1dc
Luciano Lucio Neto <lucianorlneto@gmail.com>
2024-07-16 18:00:11 -0300

    feat: adds cover letter base

---
+++ src/LucianoCoverLetter.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+
+const LucianoCoverLetter: React.FC = () => {
+  return (
+    <article className="text-left mb-8">
+      <h1 className="font-bold text-5xl mb-6">Luciano Rodrigues Lucio Neto</h1>
+      <p>Niquelândia Street, n 135, 59068-820, Natal. Brazil</p>
+      <p>Natal, Rio Grande do Norte, 59068-820</p>
+      <p>lucianorlneto@gmail.com</p>
+      <p className="mb-4">+55 84 9 9131-7498</p>
+      <p className="mb-4">Jul. 16, 2024</p>
+      <p>Chris Fei</p>
+      <p>Hiring Manager</p>
+      <p className="mb-8">mParticle</p>
+      <main>
+        <p className="font-medium text-[18px] mb-4">Dear Chris Fei,</p>
+        <p className="mb-2">
+          I am very exited to apply for the Staff Software Engineer position at
+          mParticle. I was refereed to this opportunity by Gabriel Tirbúrcio,
+          who is a Team Lead. They shared their positive experience working at
+          mParticle, and I am eager to bring my skills to your team as a
+          passionate developer whom believe in the power of programming and
+          logic to solve problems.
+        </p>
+        <p className="mb-2">
+          Over the past 6 years, I had developed solid skills in developing and
+          maintaining mobile and web applications, primarily with the React
+          framework, which I have been working with for over 5 years. I started
+          as a fullstack developer, but I have recently been focusing on my
+          frontend skills recently. My experience includes working on a variety
+          of projects such as e-commerce applications, ERP systems, personal
+          resources managing applications, services integrations, GraphQL and
+          RESTful APIs and SQL database management, using agile development
+          methodologies such as Kanban and Scrum depending on the team`s
+          workflow. Additionally, I have experience in direct client
+          interaction, managing and interpreting tasks assigned to me and
+          actively collaborating with the teams to deliver high-quality
+          solutions.
+        </p>
+        <p className="mb-4">
+          I look forward to talk about this carrer opportunity as I am looking
+          to further develop my skills and advance professionally, enabling me
+          to plan both my personal and professional life with greater enthusiasm
+          and freedom. Thank you for considering my application, and I am
+          available to further discuss how my experience can benefit your team.
+        </p>
+        <p className="mb-4">Sincerely,</p>
+        <p className="font-medium text-[18px]">Luciano Rodrigues</p>
+      </main>
+    </article>
+  );
+};
+
+export default LucianoCoverLetter;
